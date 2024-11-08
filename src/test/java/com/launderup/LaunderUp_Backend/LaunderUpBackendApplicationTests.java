24c85634eac47480fe93a095cd6e229135904538
Kavyankit Mishra <kavyankit27@gmail.com>
2024-11-08 14:32:25 +0530

    Created Project Structure and added Dependencies

---
+++ src/test/java/com/launderup/LaunderUp_Backend/LaunderUpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launderup.LaunderUp_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunderUpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
