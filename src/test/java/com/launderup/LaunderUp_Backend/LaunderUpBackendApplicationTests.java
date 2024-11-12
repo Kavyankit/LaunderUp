792130c033502e29f18de6c90aea1ed54a699db4
Kavyankit Mishra <kavyankit27@gmail.com>
2024-11-12 17:43:35 +0530

    Update LaunderUpBackendApplicationTests.java
    Naming Fixes

--- src/test/java/com/launderup/LaunderUp_Backend/LaunderUpBackendApplicationTests.java
+++ src/test/java/com/launderup/LaunderUp_Backend/LaunderUpBackendApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LaunderUpBackendClientSideApplicationTests {
+class LaunderUpBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
